1128bd9b54c0afba3a419d2678cea47ad028e5fd
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-08-01 16:22:44 -0400

    Add files via upload

---
+++ finance_data_call_example.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=DIS&apikey=E8XRSI3QLSVWSVHZ'
+r = requests.get(url)
+dis_data = r.json()
+
+# CREATE A DICTIONARY FOR THE FINANCE DATA
+symbol = dis_data['Meta Data']['2. Symbol']
+
+# GET THE DATES AND THE HIGHS AND THE LOWS
+dis_dicts = dis_data['Time Series (Daily)']
+dis_dates, highs, lows = [], [], []
+for date in dis_dicts:
+    dis_dates.append(datetime.strptime(date, '%Y-%m-%d'))
+    highs.append(float(dis_dicts[date]['2. high']))
+    lows.append(float(dis_dicts[date]['3. low']))
+
+# DEFINE REVERESE
+def reverse(lst):
+    """reverese the elements in a list"""
+    return [ele for ele in reversed(lst)]
+
+# PLOT THE HIGHS AND LOWS
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dis_dates, reverse(highs), c='red', alpha=0.6)
+ax.plot(dis_dates, reverse(lows), c='blue', alpha=0.6)
+ax.fill_between(dis_dates, reverse(highs), reverse(lows), facecolor='blue', alpha=0.15)
+
+# FORMAT PLOT
+ax.set_title(f"Daily high and low stock prices - Last 100 Days \nWalt Disney Company ({symbol})", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Price USD", fontsize=10)
+ax.tick_params(axis='both', which='major', labelsize=10)
+plt.ylim(min(lows), max(highs))
+
+plt.show()
