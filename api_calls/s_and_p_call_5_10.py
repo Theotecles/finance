ac27fa6d59973c55f233686fc03812e359811e0f
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-08-03 13:14:14 -0400

    Update s_and_p_call_5_10.py

--- api_calls/s_and_p_call_5_10.py
+++ api_calls/s_and_p_call_5_10.py
@@ -8,7 +8,7 @@ s_and_p_dicts = []
 for symbol in s_and_p_500[5:10]:
     print(symbol)
 
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey=E8XRSI3QLSVWSVHZ'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey=MYAPIKEY'
     r = requests.get(url)
     data = r.json()
     s_and_p_dicts.append(data)
